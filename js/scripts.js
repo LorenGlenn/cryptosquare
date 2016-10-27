2dba4bbbedc9d7c85c0eb2bbf4e1d642792c1fdc
Chris Clifford and Loren Glenn <chris@>
2016-10-27 13:42:56 -0700

    refactor the code to separate functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,6 @@
-var cryptoSquare = function(input) {
-  var columnNumber, rowNumber, word;
-  var finalString = "";
+//This function encrypts a string based on column and row length
+var encrypt = function(rowNumber, columnNumber, stringArray){
   var encryptedString = "";
-  var stringArray = input.replace(/[^a-zA-Z]/g, "").toLowerCase().split("");
-  var stringLength = stringArray.length;
-  var root = Math.sqrt(stringLength);
-  if (stringLength < 4) {
-    return false;
-  } else if (Number.isInteger(root)) {
-    columnNumber = root;
-    rowNumber = root;
-  } else {
-    columnNumber = parseInt(root);
-    rowNumber = parseInt(root) + 1;
-  }
-
   for (column = 0; column < columnNumber; column++) {
     for(row = 0; row < rowNumber; row++){
       if (stringArray[column + (row * columnNumber)]){
@@ -22,16 +8,37 @@ var cryptoSquare = function(input) {
       }
     }
   }
+  return encryptedString;
+}
 
+//This function adds spaces every 5 character in a string
+var addSpaces = function(encryptedString, stringLength) {
+  var finalString = "";
   for (var index = 0; index < stringLength; index += 5) {
     word = encryptedString.slice(index, (index + 5) );
     finalString = finalString + word + " ";
-    console.log(finalString);
   }
-
   return finalString;
 }
 
+//This function calculate the size of a cryptosquare and calls encrypt() and addSpaces().
+var cryptoSquare = function(input) {
+  var columnNumber, rowNumber, word;
+  var stringArray = input.replace(/[^a-zA-Z]/g, "").toLowerCase().split("");
+  var stringLength = stringArray.length;
+  var root = Math.sqrt(stringLength);
+  if (stringLength < 4) {
+    return false;
+  } else {
+    columnNumber = parseInt(root);
+    rowNumber = parseInt(root) + 1;
+  }
+  var encryptedString = encrypt(rowNumber, columnNumber, stringArray);
+  var finalString = addSpaces(encryptedString, stringLength);
+  return finalString;
+}
+
+//User interface Logic
 $(document).ready(function(){
   $("form#romans").submit(function(event){
     var userSentence = $("#user-input").val();
